930565759454f6ef78b79f9c2de3f732e231edf8
Trysten M <nick.tristen22@gmail.com>
2025-02-23 20:06:02 +0200

    add(client/components) create MailMessageHead.vue

---
+++ components/mailMessage/MailMessageHead.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  value: string,
+  styles?: string
+}>()
+</script>
+
+<template>
+  <thead
+      :style="styles"
+  >
+  {{value}}
+  </thead>
+</template>
