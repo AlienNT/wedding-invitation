dce67b872426567108b867fcfc61babc4bf3fdee
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:34:18 +0200

    add(client/components) add CountdownItem.vue

---
+++ components/UI/countdown/CountdownItem.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import CountdownItemValue from "~/components/UI/countdown/CountdownItemValue.vue";
+import CountdownItemTitle from "~/components/UI/countdown/CountdownItemTitle.vue";
+
+withDefaults(defineProps<{
+  tagName?: string,
+  value: number,
+  title: string
+}>(), {
+  tagName: 'li'
+})
+</script>
+
+<template>
+  <component
+      :is="tagName"
+      class="countdown-item"
+  >
+    <CountdownItemValue
+        :value="value"
+    />
+    <CountdownItemTitle
+        :title="title"
+    />
+  </component>
+</template>
+
+<style scoped>
+.countdown-item {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+</style>
