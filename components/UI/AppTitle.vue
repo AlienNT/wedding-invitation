79689d582a8a2007e2ab02e1be6f43a716cac9be
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:33:23 +0200

    add(client/components) add AppTitle.vue

---
+++ components/UI/AppTitle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  value: string
+}>()
+</script>
+
+<template>
+  <div class="title">
+    {{ value }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use 'assets/styles/fonts.scss';
+@use 'assets/styles/variables';
+
+.title {
+  text-align: center;
+  margin-bottom: 20px;
+  width: 100%;
+  font-family: fonts.$fontMain;
+  color: variables.$colorFontPrimary;
+  font-size: 36px;
+  font-weight: 500;
+}
+</style>
