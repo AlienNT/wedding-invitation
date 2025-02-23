d0c575026395e066074f629dfd4bc95dfaa72496
Trysten M <nick.tristen22@gmail.com>
2025-02-23 20:07:16 +0200

    add(client/components) create MailMessageTable.vue

---
+++ components/mailMessage/MailMessageTable.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  styles?: string
+}>()
+</script>
+
+<template>
+  <table
+      border="0"
+      cellpadding="0"
+      cellspacing="0"
+      style="margin:0;
+      padding:0"
+      :style="styles"
+  >
+    <slot/>
+  </table>
+</template>
