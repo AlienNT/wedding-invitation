2c72091666d71e984f3ba6bb063c40910c5317ee
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:34:12 +0200

    add(client/components) add CountdownItemValue.vue

---
+++ components/UI/countdown/CountdownItemValue.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  value: number
+}>()
+</script>
+
+<template>
+  <div class="countdown-value" :key="value">
+    {{ value || 0 }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.countdown-value {
+  font-size: 32px;
+  font-style: italic;
+}
+</style>
