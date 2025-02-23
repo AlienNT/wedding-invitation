4ba9ec1fb4e896c7d90331903d947315efec18bc
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:32:58 +0200

    add(client/components) add AppDescription.vue

---
+++ components/UI/AppDescription.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  value: string
+}>()
+</script>
+
+<template>
+<div class="description">{{value}}</div>
+</template>
+
+<style scoped>
+.description {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
