e6095b35b2ab9cae94b248e53b94ad225688833d
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:30:44 +0200

    update(client/config) add new dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: ['nuxt-mongoose', 'nuxt-mail', 'nuxt-countdown'],
+    mongoose: {
+        uri: process.env.MONGO_DB_URL
+    },
+    //@ts-ignore
+    mail: {
+        message: {
+            to: 'nick.tristen22@gmail.com'
+        },
+        smtp: {
+            service: 'gmail',
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD
+            }
+        }
+    }
 })
