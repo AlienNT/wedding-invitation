149f042a564381e499de78b844d7cb5e74de3951
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:32:49 +0200

    add(client/components) add AppButton.vue

---
+++ components/UI/AppButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  title: string,
+  disabled?: boolean,
+  type?: 'button' | 'submit' | 'reset',
+}>(), {
+  title: 'Button',
+})
+</script>
+
+<template>
+  <button
+      :type="type"
+      :disabled="disabled"
+      class="app-button"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+@use 'assets/styles/variables';
+@use 'sass:color';
+
+.app-button {
+  padding: 15px;
+  border-radius: 50px;
+  background-color: variables.$designElementColor;
+  color: variables.$colorFontLight;
+  cursor: pointer;
+  transition: background-color 0.25s ease;
+
+  &:not([disabled]):hover {
+    background-color: color.scale(variables.$designElementColor, $lightness: -20%);
+  }
+}
+</style>
