2af8f0e5ea002d4ac7dd623851d0eaec976fb7da
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:33:09 +0200

    add(client/components) add AppImage

---
+++ components/UI/AppImage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  src?: string
+  alt?: string
+}>()
+</script>
+
+<template>
+  <div class="app-image">
+    <img
+        :src="src"
+        :alt="alt"
+    >
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app-image {
+  width: 100%;
+  height: 100%;
+  background: #ccc;
+
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
