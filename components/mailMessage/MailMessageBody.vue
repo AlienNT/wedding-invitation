06a7163c1e9a3f37db6b452e2e48efefd31061dd
Trysten M <nick.tristen22@gmail.com>
2025-02-23 20:07:28 +0200

    add(client/components) create MailMessageBody.vue

---
+++ components/mailMessage/MailMessageBody.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  styles?: string
+}>()
+
+const style = computed(() => {
+  return [
+    props.styles,
+    'padding-top: 15px',
+    'padding-bottom: 15px',
+    'padding-left: 15px',
+    'padding-right: 15px',
+  ].join('; ')
+})
+</script>
+
+<template>
+  <tbody :style="style">
+  <slot/>
+  </tbody>
+</template>
