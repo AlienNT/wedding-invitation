28242cc29023138da8cebd9d251d446abd76689e
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:35:41 +0200

    add(client/components) add FormFieldTitle.vue

---
+++ components/form/FormFieldTitle.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  value: string,
+  tagName?: string,
+}>(), {
+  tagName: 'div'
+})
+</script>
+
+<template>
+  <component
+      :is="tagName"
+      class="title form-field-title"
+  >
+    {{ value }}
+  </component>
+</template>
+
+<style scoped lang="scss">
+
+</style>
