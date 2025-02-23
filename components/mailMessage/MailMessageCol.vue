06d7b2e4388a9b1155397fc24d96f7e3fcda3922
Trysten M <nick.tristen22@gmail.com>
2025-02-23 20:05:41 +0200

    add(client/components) create MailMessageCol.vue

---
+++ components/mailMessage/MailMessageCol.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: string,
+  styles?: string
+}>()
+
+const style = computed(() => {
+  return [
+    props.styles,
+    'padding-top: 5px',
+    'padding-bottom: 5px',
+    'padding-left: 15px',
+    'padding-right: 15px',
+    'font-size: 16px',
+  ].join(';')
+})
+</script>
+
+<template>
+  <td :style="style">
+    {{ value }}
+  </td>
+</template>
