2125c6b96dc33ca4d13ac1f858a1dde6f2710a26
Trysten M <nick.tristen22@gmail.com>
2025-02-23 17:34:02 +0200

    add(client/components) add CountdownItemTitle.vue

---
+++ components/UI/countdown/CountdownItemTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+
+</script>
+
+<template>
+<div class="countdown-title">
+  {{title}}
+</div>
+</template>
+
+<style scoped>
+.countdown-title {
+  font-size: 14px;
+}
+</style>
