d34e896eb5f48e7518a779c1a36b50c35c3ca591
Trysten M <nick.tristen22@gmail.com>
2025-02-23 19:44:52 +0200

    add(client/components) create SectionProgram.vue

---
+++ components/sections/SectionProgram.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import ru from "~/locales/ru";
+import AppTitle from "~/components/UI/AppTitle.vue";
+import DayProgram from "~/components/program/DayProgram.vue";
+import Observed from "~/layouts/observed.vue";
+import {useDayProgram} from "~/store/dayProgram";
+
+const {programList} = useDayProgram()
+
+</script>
+
+<template>
+  <section class="section section-program">
+    <div class="container">
+      <div class="row">
+        <Observed :step="1" transition-name="slide-right">
+          <AppTitle
+              class="col"
+              :value="ru.dayProgram.title"
+          />
+        </Observed>
+      </div>
+      <div class="row">
+        <div class="col">
+          <Observed :step="2" transition-name="slide-left">
+            <DayProgram
+                :step="3"
+                :program-list="programList"
+            />
+          </Observed>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
