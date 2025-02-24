f933f28fd94d712616fe9a983c28c935ee99567a
Trysten M <nick.tristen22@gmail.com>
2025-02-24 23:32:14 +0200

    update(client/store) update date program store

--- store/dayProgram.ts
+++ store/dayProgram.ts
@@ -5,16 +5,16 @@ const state = reactive({
         {
             _id: '1',
             time: '15:30',
-            title: 'Сбор гостей',
-            description: 'Подарите на свою улыбку и возьмите с собой хорошее настроение',
+            title: 'Збір гостей',
+            description: 'Подаруйте на свою посмішку та візьміть із собою гарний настрій',
             icon: 'assets/icons/pointer.svg',
             className: 'point'
         },
         {
             _id: '2',
             time: '16:00',
-            title: 'Церемония',
-            description: 'Может быть трогательно, разрешается всплакнуть',
+            title: 'Церемонія',
+            description: 'Може бути зворушливо, дозволяється сплакати',
             icon: 'public/icons/wedding.svg',
             className: 'ceremony'
         },
@@ -22,15 +22,15 @@ const state = reactive({
             _id: '3',
             time: '17:00',
             title: 'Банкет',
-            description: 'Самое время для вкусной еды, танцев и развлечений',
+            description: 'Саме час для смачної їжі, танців та розваг',
             icon: 'public/icons/wine.svg',
             className: 'banquet'
         },
         {
             _id: '4',
             time: '23:00',
-            title: 'Завершение торжества',
-            description: 'Надеемся, этот вечер вам понравится',
+            title: 'Завершення урочистостей',
+            description: 'Сподіваємось, цей вечір вам сподобається',
             icon: 'public/icons/fireworks.svg',
             className: 'end'
         },
