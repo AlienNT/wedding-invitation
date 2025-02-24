c1804da5ec2b8ab4c545fe7685915d62b7fc1c54
Trysten M <nick.tristen22@gmail.com>
2025-02-24 23:31:33 +0200

    update(client/utils) add date methods

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,3 +1,4 @@
+
 export function isNotEmpty<T>(value: T) {
     return value ? value.toString().trim().length > 0 : false;
 }
@@ -5,4 +6,18 @@ export function isNotEmpty<T>(value: T) {
 export function getKeys<T>(object: T) {
     if (typeof object !== 'object' || !object) return [];
     return Object.keys(object).map(key => key as keyof T);
+}
+
+export function getDate(date: string, time: string) {
+    return new Date(date + ' ' + time)
+}
+
+export function displayedDate(date: string, locales: Intl.LocalesArgument = 'uk-UA') {
+    const toDate = new Date(date);
+
+    return toDate.toLocaleDateString(locales, {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+    })
 }
