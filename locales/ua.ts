6860ed69baf4a1ac6a5ef3dca0c58985e73b2b2c
Trysten M <nick.tristen22@gmail.com>
2025-02-24 23:28:56 +0200

    add(client/locales) add ua locale

---
+++ locales/ua.ts
@@ -0,0 +1,38 @@
+import type {LocaleInterface} from "#shared/types/locale";
+
+export default {
+    newlyweds: {
+        husband: {
+            name:"Андрій"
+        },
+        and: 'та',
+        wife: {
+            name: 'Аліна'
+        }
+    },
+    invite: {
+        title: 'запрошуємо на наше весілля!'
+    },
+    footer: {
+        title: 'Будемо вас чекати',
+        description: 'З усіх питань, що вас цікавлять, ви можете зв\'язатися з нашим весільним організатором',
+    },
+    countdown: {
+        title: 'До весілля залишилось'
+    },
+    presence: {
+        title: 'Ваша присутність',
+        description: 'Підтвердьте свою присутність і дайте відповідь на кілька питань. Це допоможе в організації урочистостей! Чекатимемо вашу відповідь до 01.09.2024'
+    },
+    location: {
+        title: 'Локація',
+        description: '',
+        link: '',
+        buttonTitle: 'Перейти на карту',
+        name: 'Bellagio banquet hall',
+        address: 'м. Краматорськ, вул. Софіївська 1/1'
+    },
+    dayProgram: {
+        title: 'Програма дня',
+    }
+} as LocaleInterface
