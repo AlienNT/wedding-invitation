39e8e02899d2744760a6e0dc458f1d0a37e4fdd6
Trysten M <nick.tristen22@gmail.com>
2025-07-12 17:12:33 +0300

    update footer

--- components/sections/SectionFooter.vue
+++ components/sections/SectionFooter.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 
 import AppTitle from "~/components/UI/AppTitle.vue";
-import AppDescription from "~/components/UI/AppDescription.vue";
 import SectionBanner from "~/components/UI/SectionBanner.vue";
-import FooterImage from "assets/images/location.webp"
+import FooterImage from "assets/images/bottom-bg.jpg"
 import Observed from "~/layouts/observed.vue";
-import {useContacts} from "~/store/contacts";
 
-const {contacts} = useContacts()
 const {locale} = useLocale()
 
 </script>
@@ -15,29 +12,6 @@ const {locale} = useLocale()
 <template>
   <section class="section section-footer">
     <div class="container container--fluid">
-      <div class="row">
-        <div class="col description-col">
-          <Observed :step="1">
-            <AppDescription
-                :value="locale.footer.description"
-            />
-          </Observed>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col contacts-col">
-          <template v-for="({title, value}, index ) in contacts">
-            <Observed :step="2 + index">
-              <a
-                  :href="value"
-                  class="href"
-              >
-                {{ title }}
-              </a>
-            </Observed>
-          </template>
-        </div>
-      </div>
       <div class="row">
         <div class="col title-col">
           <Observed :step="3">
